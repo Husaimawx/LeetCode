f2381638d3c27296c330f73f88fc90157112df62
yukeyi <yukeyi14@163.com>
2018-06-22 23:56:16 +0800

    6/22

---
+++ 257 Binary Tree Paths/Binary Tree Paths.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> path;
+    vector<string> res;
+    
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        if(root == NULL)
+            return res;
+        binaryTreePathsAux(root);
+        
+        return res;
+    }
+    
+    void binaryTreePathsAux(TreeNode* root)
+    {
+        if(root == NULL)
+        {
+            string temp = to_string(path[0]);
+            for(int i = 1;i<path.size();i++)
+            {
+                temp = temp + "->" + to_string(path[i]);
+            }
+            res.push_back(temp);
+            return;    
+        }
+        
+        path.push_back(root->val);
+        binaryTreePathsAux(root->left);
+        binaryTreePathsAux(root->right);
+        path.pop_back();
+        return;
+    }
+    
+};
