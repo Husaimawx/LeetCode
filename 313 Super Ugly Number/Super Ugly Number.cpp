606260f5aa9ecab4d112668868d11dd1e2fe69b2
yukeyi <yukeyi14@163.com>
2018-08-07 11:16:15 +0800

    8/7

---
+++ 313 Super Ugly Number/Super Ugly Number.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int nthSuperUglyNumber(int n, vector<int>& primes) {
+        vector<int> answer(1,1);
+        if(n == 1)
+            return 1;
+        
+        vector<int> pointer(primes.size(), 0);
+        while(answer.size() != n)
+        {
+            int min_index = 0;
+            int min_value = INT_MAX;
+            int temp;
+            for(int j = 0; j<primes.size(); j++)
+            {
+                temp = primes[j]*answer[pointer[j]];
+                if(temp == answer[answer.size()-1])
+                {
+                    pointer[j]++;
+                    j--;
+                    continue;
+                }
+                if(temp < min_value)
+                {
+                    min_value = temp;
+                    min_index = j;
+                }
+            }
+            answer.push_back(min_value);
+            pointer[min_index] += 1;
+        }
+        
+        return answer[n-1];
+    }
+};
