950da83703fc72f6bf0b21d2e28d58c5e892c0b9
yukeyi <yukeyi14@163.com>
2018-10-01 21:39:07 -0400

    10/1

---
+++ 698 Partition to K Equal Sum Subsets/Partition to K Equal Sum Subsets.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int sum = 0;
+        for(int i = 0;i<nums.size();i++)
+        {
+            sum += nums[i];
+        }
+        if(sum % k != 0)
+            return false;
+        
+        vector<bool> visited(nums.size(),true); 
+        return helper(nums, k, sum/k, 0, 0, visited);
+    }
+    bool helper(vector<int>& nums, int k, int target, int start, int curSum, vector<bool>& visited) {
+        if(k == 1)
+            return true;
+        if(curSum == target)
+            return helper(nums, k-1, target, 0, 0, visited);
+        for(int i = start;i<nums.size();i++)
+        {
+            if(visited[i] == false)
+                continue;
+            if(nums[i] + curSum > target)
+                break;
+            visited[i] = false;
+            if(helper(nums, k, target, i+1, curSum+nums[i], visited))
+                return true;
+            visited[i] = true;
+        }
+        return false;
+    }
+};
