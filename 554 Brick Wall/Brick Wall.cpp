db8aeba6d630685603e884954bf3605b1f3b038f
yukeyi <yukeyi14@163.com>
2018-10-11 16:19:56 -0400

    10/11

---
+++ 554 Brick Wall/Brick Wall.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int leastBricks(vector<vector<int>>& wall) {
+        unordered_map<int,int> M;
+        int max = 0;
+        for(int i = 0;i<wall.size();i++)
+        {
+            int sum = 0;
+            for(int j = 0;j<wall[i].size()-1;j++)
+            {
+                sum += wall[i][j];
+                if(M.count(sum) == 0)
+                    M[sum] = 1;
+                else
+                    M[sum] += 1;
+                if(max < M[sum])
+                    max = M[sum];
+            }
+        }
+        return int(wall.size())-max;
+    }
+};
