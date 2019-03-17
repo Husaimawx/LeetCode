b6fe3bdb1f789fafe69b68652eea88540bd44055
Keyi Yu <yukeyi@Keyis-MBP.fios-router.home>
2019-03-17 17:04:28 -0400

    3/17

---
+++ 739 Daily Temperatures/Daily Temperatures.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& T) {
+        vector<int> res(T.size(),0);
+        
+        stack<int> st;
+        st.push(0);
+        
+        for(int i = 1;i<T.size();i++) {
+            while(st.size() != 0 && T[st.top()] < T[i]) {
+                res[st.top()] = i-st.top();
+                st.pop();
+            }
+            st.push(i);
+        }
+        
+        return res;
+    }
+};
